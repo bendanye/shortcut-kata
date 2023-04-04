6192b5a9c5dadceec551d878ec84b27acd31ac23
Ng Shen Soon Benjamin <shen_soonng@hotmail.com>
2023-04-04 16:19:19 +0800

    Added time taken and output to a file

--- terminal/start.sh
+++ terminal/start.sh
@@ -1,10 +1,29 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( dirname -- "$0"; )
+
+DATA_FILE="$SCRIPT_DIR/time_taken.txt"
+
+if ! test -f "$DATA_FILE"; then
+    echo "start_date;time_taken_in_secs" > $DATA_FILE
+fi
+
 start_line_num=5
 stop_line_num=14
+
+START=$(date +%s)
 
 for((i=$start_line_num;i<=$stop_line_num;++i)) do
     clear
     awk "NR == $i" README.md
     read -p "Press any keys to continue"
 done
+
+END=$(date +%s)
+secs=$((END-START))
+
+START_FORMATTED=$(date -r $START '+%Y-%m-%d %H:%M:%S')
+
+echo "$START_FORMATTED;$secs" >> $DATA_FILE
 
 echo "Shortcut kata (terminal) Completed!"
