f95209dc98c5ff447ed8fb34f4ae98db71da4769
bendanye <shen_soonng@hotmail.com>
2023-05-01 08:56:15 +0800

    Simple script to log timing

---
+++ vi/start.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( dirname -- "$0"; )
+
+DATA_FILE="$SCRIPT_DIR/time_taken.txt"
+
+if ! test -f "$DATA_FILE"; then
+    echo "start_date,time_taken_in_secs" > $DATA_FILE
+fi
+
+clear
+awk "NR == 5" $SCRIPT_DIR/README.md
+read -p "Press any keys to start"
+
+start_line_num=6
+stop_line_num=24
+
+START=$(date +%s)
+
+for((i=$start_line_num;i<=$stop_line_num;++i)) do
+    clear
+    awk "NR == $i" $SCRIPT_DIR/README.md
+    read -p "Press any keys to continue"
+done
+
+END=$(date +%s)
+secs=$((END-START))
+
+START_FORMATTED=$(date -r $START '+%Y-%m-%d %H:%M:%S')
+
+echo "$START_FORMATTED,$secs" >> $DATA_FILE
+
+echo "Shortcut kata (vi) completed in $secs seconds!"
