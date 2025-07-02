30906840329fa7bf7f6da6c4a02f4c66bdf9fca7
bendanye <shen_soonng@hotmail.com>
2025-07-02 19:23:26 +0800

    Set the directory

--- start_all_exclude_ide.sh
+++ start_all_exclude_ide.sh
@@ -1,4 +1,8 @@
-./terminal/start.sh
-./vi/start.sh
-./os/start.sh
-python3 ./browser/start.py
+#!/bin/bash
+
+SCRIPT_DIR=$( dirname -- "$0"; )
+
+sh "$SCRIPT_DIR/terminal/start.sh"
+sh "$SCRIPT_DIR/start.sh"
+sh "$SCRIPT_DIR/os/start.sh"
+python3 "$SCRIPT_DIR/browser/start.py"
